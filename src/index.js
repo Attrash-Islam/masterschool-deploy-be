a85aa44b5cc0a577d48d07eaeebeb502f34bfa61
Islam Attrash <isattrash@gmail.com>
2023-01-17 20:19:53 +0200

    last fix

--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ app.get("/", function(req, res) {
   res.send({ data: [1,2,3]});
 });
 
-app.listen(80, function() {
-  console.log("server running on 80");
+app.listen(8080, function() {
+  console.log("server running on 8080");
 }); //the server object listens on port 8080
