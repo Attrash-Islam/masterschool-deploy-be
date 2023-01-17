e71859293a2e552d65d94d75fe0137803859ca2d
Islam Attrash <isattrash@gmail.com>
2023-01-17 20:25:39 +0200

    ddd

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const app = express();
 
 //create a server object:
 app.get("/", function(req, res) {
-  res.send({ data: [1,2,3]});
+  res.send({ data: [1,2,3] });
 });
 
 app.listen(80, function() {
-  console.log("server running on 8080");
+  console.log("server running on 80");
 }); //the server object listens on port 8080
