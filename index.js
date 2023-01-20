03153ac5fd6feb9018efd6b737b7bde21236bdaa
Islam Attrash <isattrash@gmail.com>
2023-01-20 12:20:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,24 +2,16 @@ const express = require("express");
 const app = express();
 const { MongoClient } = require('mongodb');
 
-const whitelist = [
-  '*'
-];
-
 const url = 'mongodb+srv://admin:admin@cluster0.egal9tt.mongodb.net/final_project';
 
 const client = new MongoClient(url);
 const dbName = 'final_project';
 
 app.use((req, res, next) => {
-  const origin = req.get('referer');
-  const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
-  if (isWhitelisted) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-  }
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   // Pass to next layer of middleware
   if (req.method === 'OPTIONS') res.sendStatus(200);
   else next();
